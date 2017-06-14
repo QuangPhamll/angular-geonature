0617a2e7629d8daecb0509c7f70c37359a6ea2e0
QuangPhamll <quang.pham.fr@gmail.com>
2017-06-14 17:29:49 +0200

    Creat draft form contact faune

--- src/app/contact-faune/contact-faune-formulaire/contact-faune-formulaire.component.ts
+++ src/app/contact-faune/contact-faune-formulaire/contact-faune-formulaire.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit, NgModule } from '@angular/core';
 })
 
 export class ContactFauneFormulaireComponent implements OnInit {
-
+  selectedLangue: string;
+  langues = [
+    {value: 'francais', viewValue: 'Francais'},
+    {value: 'latin', viewValue: 'Latin'}
+  ];
+  selectedTaxonomique: string;
+  taxonomiques = [
+    {value: 'Mammifières', viewValue: 'Mammifières'},
+    {value: 'Oiseaux', viewValue: 'Oiseaux'},
+    {value: 'Reptiles', viewValue: 'Reptiles'},
+    {value: 'Amphibiens', viewValue: 'Amphibiens'},
+    {value: 'Poissions', viewValue: 'Poissions'}
+    ];
+  taxons = [
+    {value: 'Mammifières', viewValue: 'Mammifières'},
+    {value: 'Oiseaux', viewValue: 'Oiseaux'},
+    {value: 'Reptiles', viewValue: 'Reptiles'},
+    {value: 'Amphibiens', viewValue: 'Amphibiens'},
+    {value: 'Poissions', viewValue: 'Poissions'}
+  ]
   constructor() { }
 
   ngOnInit() {
