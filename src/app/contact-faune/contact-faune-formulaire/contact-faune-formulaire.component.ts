e09c779c741cc794de47ec5de240234f8bbad1ea
QuangPhamll <quang.pham.fr@gmail.com>
2017-06-15 17:59:39 +0200

    Add API query data of Marker

--- src/app/contact-faune/contact-faune-formulaire/contact-faune-formulaire.component.ts
+++ src/app/contact-faune/contact-faune-formulaire/contact-faune-formulaire.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import MapService from '../../services/map.service';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-contact-faune-formulaire',
@@ -9,11 +10,13 @@ import MapService from '../../services/map.service';
 
 export class ContactFauneFormulaireComponent implements OnInit {
   selectedLangue: string;
+  private httpDone = false;
   langues = [
     {value: 'francais', viewValue: 'Francais'},
     {value: 'latin', viewValue: 'Latin'}
   ];
   public result;
+  public gpsData;
   selectedTaxonomique: string;
   taxonomiques = [
     {value: 'Mammifières', viewValue: 'Mammifières'},
@@ -29,7 +32,7 @@ export class ContactFauneFormulaireComponent implements OnInit {
     {value: 'Amphibiens', viewValue: 'Amphibiens'},
     {value: 'Poissions', viewValue: 'Poissions'}
   ]
-  constructor(private mapService: MapService) {
+  constructor(private mapService: MapService, private http: Http) {
     this.result = {
       type : '',
       coordinates: []
@@ -39,9 +42,19 @@ export class ContactFauneFormulaireComponent implements OnInit {
   ngOnInit() {
   }
   showResult() {
+    this.httpDone = false;
     let result;
+    // tslint:disable-next-line:prefer-const
     result = this.mapService.marker.toGeoJSON();
     this.result.type = result.geometry.type;
     this.result.coordinates = result.geometry.coordinates;
+    // tslint:disable-next-line:max-line-length
+    this.http.get('http://nominatim.openstreetmap.org/reverse?format=json&lat=' + encodeURIComponent(this.result.coordinates[1]) + '&lon=' + encodeURIComponent(this.result.coordinates[0]) + '&zoom=18&addressdetails=1)')
+    .map(res => res.json())
+    .subscribe(results => {
+      this.gpsData = results;
+      this.httpDone = true;
+      console.log(this.gpsData);
+      });
   }
 }
